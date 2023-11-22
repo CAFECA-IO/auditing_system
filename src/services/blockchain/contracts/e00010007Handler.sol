977ff494028065bd4f9f515623937b1c46ec8ad1
Chen Pin-Yang <59311328+YangIsCoding@users.noreply.github.com>
2023-11-22 16:58:31 +0800

    Update e00010007Handler.sol

--- src/services/blockchain/contracts/e00010007Handler.sol
+++ src/services/blockchain/contracts/e00010007Handler.sol
@@ -75,79 +75,50 @@ contract E00010007Handler is ITransactionHandler {
     function computeBalanceSheet() internal  {
 
         int256 A001_2_3_4_5_14 =  EP001 + EP003;
-        string[] memory keysForA001_2_3_4_5_14 = new string[](6);
-        keysForA001_2_3_4_5_14[0] = "assets.details.cashAndCashEquivalent.totalAmountFairValue";
-        keysForA001_2_3_4_5_14[1] = "assets.details.cashAndCashEquivalent.breakdown.USD.amount";
-        keysForA001_2_3_4_5_14[2] = "assets.details.cashAndCashEquivalent.breakdown.USD.fairValue";
-        keysForA001_2_3_4_5_14[3] = "assets.totalAmountFairValue";
-        keysForA001_2_3_4_5_14[4] = "totalAssetsFairValue";
-        keysForA001_2_3_4_5_14[5] = "totalLiabilitiesAndEquityFairValue";
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[0], A001_2_3_4_5_14);
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[1], A001_2_3_4_5_14);
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[2], A001_2_3_4_5_14);
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[3], A001_2_3_4_5_14);
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[4], A001_2_3_4_5_14);
-        report.addValue(reportID, "balanceSheet", keysForA001_2_3_4_5_14[5], A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "assets.details.cashAndCashEquivalent.totalAmountFairValue", A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "assets.details.cashAndCashEquivalent.breakdown.USD.amount", A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "assets.details.cashAndCashEquivalent.breakdown.USD.fairValue", A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "assets.totalAmountFairValue", A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "totalAssetsFairValue", A001_2_3_4_5_14);
+        report.addValue(reportID, "balanceSheet", "totalLiabilitiesAndEquityFairValue", A001_2_3_4_5_14);
 
         int256 A006_7_8_9 =  EP001 - EP002;
-        string[] memory keysForA006_7_8_9 = new string[](4);
-        keysForA006_7_8_9[0] = "liabilities.details.userDeposit.totalAmountFairValue";
-        keysForA006_7_8_9[1] = "liabilities.details.userDeposit.breakdown.USD.amount";
-        keysForA006_7_8_9[2] = "liabilities.details.userDeposit.breakdown.USD.fairValue";
-        keysForA006_7_8_9[3] = "liabilities.totalAmountFairValue";
-        report.addValue(reportID, "balanceSheet", keysForA006_7_8_9[0], A006_7_8_9);
-        report.addValue(reportID, "balanceSheet", keysForA006_7_8_9[1], A006_7_8_9);
-        report.addValue(reportID, "balanceSheet", keysForA006_7_8_9[2], A006_7_8_9);
-        report.addValue(reportID, "balanceSheet", keysForA006_7_8_9[3], A006_7_8_9);
-        report.addValue(reportID, "balanceSheet", keysForA006_7_8_9[4], A006_7_8_9);
+        report.addValue(reportID, "balanceSheet", "liabilities.details.userDeposit.totalAmountFairValue", A006_7_8_9);
+        report.addValue(reportID, "balanceSheet", "liabilities.details.userDeposit.breakdown.USD.amount", A006_7_8_9);
+        report.addValue(reportID, "balanceSheet", "liabilities.details.userDeposit.breakdown.USD.fairValue", A006_7_8_9);
+        report.addValue(reportID, "balanceSheet", "liabilities.details.userDeposit.breakdown.USD.fairValue", A006_7_8_9);
+        report.addValue(reportID, "balanceSheet", "liabilities.totalAmountFairValue", A006_7_8_9);
 
 
         int256 A010_11 =  EP002 + EP003;
         string[] memory keysForA010_11 = new string[](2);
         keysForA010_11[0] = "equity.details.retainedEarnings.totalAmountFairValue";
         keysForA010_11[1] = "equity.details.retainedEarnings.breakdown.USD.amount";
-        report.addValue(reportID, "balanceSheet", keysForA010_11[0], A010_11);
-        report.addValue(reportID, "balanceSheet", keysForA010_11[1], A010_11);
-        report.addValue(reportID, "balanceSheet", keysForA010_11[2], A010_11);
+        report.addValue(reportID, "balanceSheet", "equity.details.retainedEarnings.totalAmountFairValue", A010_11);
+        report.addValue(reportID, "balanceSheet", "equity.details.retainedEarnings.breakdown.USD.amount", A010_11);
     }
 
     function computeComprehesiveIncome() internal {
         int256 B001_2_3_4 = EP002 + EP003;
-        string[] memory keysForB001_2_3_4 = new string[](4);
-        keysForB001_2_3_4[0] = "income.details.depositFee.weightedAverageCost";
-        keysForB001_2_3_4[1] = "income.details.depositFee.breakdown.USD.amount";
-        keysForB001_2_3_4[2] = "income.details.depositFee.breakdown.USD.weightedAverageCost";
-        keysForB001_2_3_4[3] = "netProfit";
-        report.addValue(reportID, "comprehensiveIncome", keysForB001_2_3_4[0], B001_2_3_4);
-        report.addValue(reportID, "comprehensiveIncome", keysForB001_2_3_4[1], B001_2_3_4);
-        report.addValue(reportID, "comprehensiveIncome", keysForB001_2_3_4[2], B001_2_3_4);
-        report.addValue(reportID, "comprehensiveIncome", keysForB001_2_3_4[3], B001_2_3_4);
+        report.addValue(reportID, "comprehensiveIncome", "income.details.depositFee.weightedAverageCost", B001_2_3_4);
+        report.addValue(reportID, "comprehensiveIncome", "income.details.depositFee.breakdown.USD.amount", B001_2_3_4);
+        report.addValue(reportID, "comprehensiveIncome", "income.details.depositFee.breakdown.USD.weightedAverageCost", B001_2_3_4);
+        report.addValue(reportID, "comprehensiveIncome", "netProfit", B001_2_3_4);
     }
 
     function computeCashFlow() internal {
         int256 C027_134_135 = int256(EP001 - EP002);
-        string[] memory keysForC027_134_135= new string[](3);
-        keysForC027_134_135[0] = "operatingActivities.details.cashDepositedByCustomers.weightedAverageCost"; 
-        keysForC027_134_135[1] = "operatingActivities.details.cashDepositedByCustomers.breakdown.USD.amount";
-        keysForC027_134_135[2] = "operatingActivities.details.cashDepositedByCustomers.breakdown.USD.weightedAverageCost";
-        report.addValue(reportID, "cashFlow", keysForC027_134_135[0], C027_134_135);
-        report.addValue(reportID, "cashFlow", keysForC027_134_135[1], C027_134_135);
-        report.addValue(reportID, "cashFlow", keysForC027_134_135[2], C027_134_135);
-//
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashDepositedByCustomers.weightedAverageCost", C027_134_135);
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashDepositedByCustomers.breakdown.USD.amount", C027_134_135);
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashDepositedByCustomers.breakdown.USD.weightedAverageCost", C027_134_135);
+
         int256 C031_136 = int256(EP002 + EP003);
-        string[] memory keysForC031_136 = new string[](2);
-        keysForC031_136[0] = "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.weightedAverageCost"; 
-        keysForC031_136[1] = "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.breakdown.USD.amount ";
-        report.addValue(reportID, "cashFlow", keysForC031_136[0], C031_136);
-        report.addValue(reportID, "cashFlow", keysForC031_136[1], C031_136);
-//
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.weightedAverageCost", C031_136);
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.breakdown.USD.amount", C031_136);
+
         int256 C049_051_137 = int256(EP001 - EP002);
-        string[] memory keysForC049_051_137= new string[](3);
-        keysForC049_051_137[0] = "operatingActivities.weightedAverageCost"; 
-        keysForC049_051_137[1] = "otherSupplementaryItems.details.relatedToCash.cashCashEquivalentsAndRestrictedCashEndOfPeriod.weightedAverageCost";
-        keysForC049_051_137[2] = "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.breakdown.USD.weightedAverageCost";
-        report.addValue(reportID, "cashFlow", keysForC049_051_137[0], C049_051_137);
-        report.addValue(reportID, "cashFlow", keysForC049_051_137[1], C049_051_137);
-        report.addValue(reportID, "cashFlow", keysForC049_051_137[2], C049_051_137);
+        report.addValue(reportID, "cashFlow", "operatingActivities.weightedAverageCost", C049_051_137);
+        report.addValue(reportID, "cashFlow", "otherSupplementaryItems.details.relatedToCash.cashCashEquivalentsAndRestrictedCashEndOfPeriod.weightedAverageCost", C049_051_137);
+        report.addValue(reportID, "cashFlow", "operatingActivities.details.cashReceivedFromCustomersAsTransactionFee.breakdown.USD.weightedAverageCost", C049_051_137);
     }
 }
