9c1a276f1c70c4fbb0459193ce3da7e8628c510a
Chen Pin-Yang <59311328+YangIsCoding@users.noreply.github.com>
2023-12-01 11:10:52 +0800

    Update router.sol

--- src/services/blockchain/contracts/router.sol
+++ src/services/blockchain/contracts/router.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./transactionContract.sol";
 import "./getTransactionTimeSpan.sol";
+import "./reports.sol";
 import "../interfaces/iTransactionHandler.sol";
 
 //Info:(20231115-Yang){This contract provides a clean interface for users to manipulate}
@@ -10,10 +11,12 @@ contract RouterContract {
     
     TransactionContract private transactionContract;
     GetTransactionTimeSpan private timeSpanReport;
+    Reports private reports;
 
-    constructor(address _transactionContract, address _timeSpanReport) {
+    constructor(address _transactionContract, address _timeSpanReport, address _reports) {
         transactionContract = TransactionContract(_transactionContract);
         timeSpanReport = GetTransactionTimeSpan(_timeSpanReport);
+        reports = Reports(_reports);
     }
     //Info:(20231115-Yang){User should first input transaction type and handler addresses in order to register handlers}
     function registerHandler(bytes32 transactionType, address handlerAddress) external {
@@ -28,15 +31,18 @@ contract RouterContract {
     function setRate(bytes32 _SP002, bytes32 _SP003, bytes32 _SP004, bytes32 _reportID) external {
         timeSpanReport.setRate(_SP002, _SP003, _SP004, _reportID);
     }
-     //Info:(20231115-Yang){Users can set a time span and reportID to get events within the time span}
+    //Info:(20231115-Yang){Users can set a time span and reportID to get events within the time span}
     function generateReport(int256 startTime, int256 endTime, bytes32 reportID) external {
         timeSpanReport.filterTransactionsInRange(startTime, endTime, reportID);
     }
-
+    //Info:(20231115-Yang){Users can read the latest transaction time}
     function getLatestTransactionTime() external view returns (int256) {
         return transactionContract.getLatestTransactionTime();
     }
-
+    //Info:(20231201-Yang){User can read reports columns}
+    function getValue(string memory reportID, string memory reportType, string memory reportColumn)external view returns(int256){
+        return reports.getValue(reportID, reportType, reportColumn);
+    }
     function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
