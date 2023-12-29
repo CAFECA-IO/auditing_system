0b0008ac251997c3a6b466405c08324f25f00684
yang <allanustw@gmail.com>
2023-12-29 16:54:44 +0800

    12/29

--- src/services/blockchain/contracts/report_nft.sol
+++ src/services/blockchain/contracts/report_nft.sol
@@ -2,49 +2,74 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "../interfaces/IERC721Extended.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "../interfaces/ierc_isuncloud.sol";
 
-contract ReportNFT is IERC721Extended {
+contract ReportNFT is IERC_ISUNCLOUD {
+
     uint256 private _tokenIdCounter;
     string private _name;
     string private _symbol;
-
-
-    struct Report {
-        string name;
-        uint256 startTime;
-        uint256 endTime;
-    }
-
-
     mapping(uint256 => Report) private _reports;
     mapping(uint256 tokenId => address) private _owners;
     mapping(address owner => uint256) private _balances;
     mapping(uint256 tokenId => address) private _tokenApprovals;
     mapping(address owner => mapping(address operator => bool)) private _operatorApprovals;
     bytes4 private constant _ERC721_INTERFACE_ID = 0x80ac58cd;
+    bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     mapping(bytes4 => bool) private _supportedInterfaces;
     string private _collectionName;
     string private _collectionSymbol;
-
     address private _owner;
 
 
-    event ReportNFTMinted(address recipient, uint256 tokenId, string reportName, uint256 startTime, uint256 endTime);
-
-
     constructor(string memory collectionName, string memory collectionSymbol) {
-        _registerInterface(0x80ac58cd); // ERC721 interface id
-        _registerInterface(0x5b5e139f);
+        _registerInterface(_ERC721_INTERFACE_ID);
+        _registerInterface(_INTERFACE_ID_ERC721_METADATA);
         _collectionName = collectionName;
         _collectionSymbol = collectionSymbol;
         _owner = msg.sender;
         _tokenIdCounter = 0;
     }
 
-     function supportsInterface(bytes4 interfaceId) public view returns (bool) {
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Caller is not the owner");
+        _;
+    }
+
+    function uintToString(uint256 value) internal pure  returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        uint256 index = digits - 1;
+        temp = value;
+        while (temp != 0) {
+            buffer[index--] = bytes1(uint8(48 + temp % 10));
+            temp /= 10;
+        }
+
+        return string(buffer);
+    }
+
+    function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
+        return _owners[tokenId];
+    }
+
+    function name() public view override returns (string memory) {
+        return _collectionName;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _collectionSymbol;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
         return _supportedInterfaces[interfaceId];
     }
 
@@ -54,15 +79,25 @@ contract ReportNFT is IERC721Extended {
     }
 
 
-    function balanceOf(address tokenOwner) public view virtual returns (uint256) {
+    function balanceOf(address tokenOwner) public view virtual override returns (uint256) {
         if (tokenOwner == address(0)) {
             revert ERC721InvalidOwner(address(0));
         }
         return _balances[tokenOwner];
     }
 
+    function _mint(address to, uint256 tokenId) internal {
+        if (to == address(0)) {
+            revert ERC721InvalidReceiver(address(0));
+        }
+        address previousOwner = _update(to, tokenId, address(0));
+        if (previousOwner != address(0)) {
+            revert ERC721InvalidSender(address(0));
+        }
+    }
 
-    function mintReportNFT(address recipient, uint256 startTime, uint256 endTime, string memory reportName) external onlyOwner returns (uint256) {
+
+    function mintReportNFT(address recipient, uint256 startTime, uint256 endTime, string memory reportName) external override onlyOwner returns (uint256) {
         require(recipient != address(0), "Recipient address cannot be zero");
         require(endTime > startTime, "EndTime must be greater than StartTime");
 
@@ -77,6 +112,14 @@ contract ReportNFT is IERC721Extended {
         return newItemId;
     }
 
+    function _safeMint(address to, uint256 tokenId) internal {
+        _safeMint(to, tokenId, "");
+    }
+
+    function _safeMint(address to, uint256 tokenId, bytes memory data) internal virtual {
+        _mint(to, tokenId);
+        _checkOnERC721Received(address(0), to, tokenId, data);
+    }
 
 
     function transferFrom(address from, address to, uint256 tokenId) public virtual {
@@ -101,101 +144,49 @@ contract ReportNFT is IERC721Extended {
         }
     }
 
-    function tokenURI(uint256 tokenId) public view virtual returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override virtual returns (string memory) {
         _requireOwned(tokenId);
 
         string memory baseURI = _baseURI();
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, uintToString(tokenId))) : "";
     }
 
-    function name() public view returns (string memory) {
-        return _collectionName;
+    function _baseURI() internal view virtual returns (string memory) {
+        return "";
     }
 
-    function symbol() public view returns (string memory) {
-        return _collectionSymbol;
-    }
-
-    function uintToString(uint256 value) internal pure returns (string memory) {
-    if (value == 0) {
-        return "0";
-    }
-
-
-
-    uint256 temp = value;
-    uint256 digits;
-    while (temp != 0) {
-        digits++;
-        temp /= 10;
-    }
-
-    bytes memory buffer = new bytes(digits);
-    uint256 index = digits - 1;
-    temp = value;
-    while (temp != 0) {
-        buffer[index--] = bytes1(uint8(48 + temp % 10));
-        temp /= 10;
-    }
-
-    return string(buffer);
-}
-
-    function safeTransferFrom(address from, address to, uint256 tokenId) public {
+    function safeTransferFrom(address from, address to, uint256 tokenId) override public {
         safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override virtual {
+        transferFrom(from, to, tokenId);
+        _checkOnERC721Received(from, to, tokenId, data);
     }
 
     function _safeTransfer(address from, address to, uint256 tokenId) internal {
         _safeTransfer(from, to, tokenId, "");
     }
 
-    /**
-     * @dev Same as {xref-ERC721-_safeTransfer-address-address-uint256-}[`_safeTransfer`], with an additional `data` parameter which is
-     * forwarded in {IERC721Receiver-onERC721Received} to contract recipients.
-     */
     function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal virtual {
         _transfer(from, to, tokenId);
         _checkOnERC721Received(from, to, tokenId, data);
     }
 
 
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual {
-        transferFrom(from, to, tokenId);
-        _checkOnERC721Received(from, to, tokenId, data);
-    }
-
-    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory data) private {
-        if (to.code.length > 0) {
-            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, data) returns (bytes4 retval) {
-                if (retval != IERC721Receiver.onERC721Received.selector) {
-                    revert ERC721InvalidReceiver(to);
-                }
-            } catch (bytes memory reason) {
-                if (reason.length == 0) {
-                    revert ERC721InvalidReceiver(to);
-                } else {
-                    /// @solidity memory-safe-assembly
-                    assembly {
-                        revert(add(32, reason), mload(reason))
-                    }
-                }
-            }
-        }
-    }
-
-    function _baseURI() internal view virtual returns (string memory) {
-        return "";
-    }
 
     function _approve(address to, uint256 tokenId, address auth) internal {
         _approve(to, tokenId, auth, true);
+    }
+
+    function isApprovedForAll(address tokenOwner, address operator) public view virtual returns (bool) {
+        return _operatorApprovals[tokenOwner][operator];
     }
 
     function _approve(address to, uint256 tokenId, address auth, bool emitEvent) internal virtual {
         // Avoid reading the owner unless necessary
         if (emitEvent || auth != address(0)) {
             address tokenOwner = _requireOwned(tokenId);
-
             // We do not use _isAuthorized because single-token approvals should not be able to call approve
             if (auth != address(0) && tokenOwner != auth && !isApprovedForAll(tokenOwner, auth)) {
                 revert ERC721InvalidApprover(auth);
@@ -207,15 +198,6 @@ contract ReportNFT is IERC721Extended {
         }
 
         _tokenApprovals[tokenId] = to;
-    }
-
-    function _safeMint(address to, uint256 tokenId) internal {
-        _safeMint(to, tokenId, "");
-    }
-
-    function _safeMint(address to, uint256 tokenId, bytes memory data) internal virtual {
-        _mint(to, tokenId);
-        _checkOnERC721Received(address(0), to, tokenId, data);
     }
 
     function _increaseBalance(address account, uint128 value) internal virtual {
@@ -247,12 +229,7 @@ contract ReportNFT is IERC721Extended {
         return tokenOwner;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "Caller is not the owner");
-        _;
-    }
-
-    function ownerOf(uint256 tokenId) public view  returns (address) {
+    function ownerOf(uint256 tokenId) public override view  returns (address) {
         address tokenOwner = _owners[tokenId];
         require(tokenOwner != address(0), "ERC721: owner query for nonexistent token");
         return tokenOwner;
@@ -262,7 +239,7 @@ contract ReportNFT is IERC721Extended {
         return _owner;
     }
 
-    function getReportData(uint256 tokenId) external view returns (Report memory) {
+    function getReportData(uint256 tokenId) external override view returns (Report memory) {
         require(
             ownerOf(tokenId) == msg.sender || msg.sender == owner(),
             "Caller doesn't have the authority to access the token. Caller must be the NFT contract deployer or the token owner."
@@ -271,35 +248,22 @@ contract ReportNFT is IERC721Extended {
         return _reports[tokenId];
     }
 
-    function getLatestTokenID() external view returns (uint256) {
+    function getLatestTokenID() external override view returns (uint256) {
         return _tokenIdCounter;
     }
 
-    function _mint(address to, uint256 tokenId) internal {
-        if (to == address(0)) {
-            revert ERC721InvalidReceiver(address(0));
-        }
-        address previousOwner = _update(to, tokenId, address(0));
-        if (previousOwner != address(0)) {
-            revert ERC721InvalidSender(address(0));
-        }
-    }
-
-    function approve(address to, uint256 tokenId) public virtual {
+    function approve(address to, uint256 tokenId) override public virtual {
         _approve(to, tokenId, msg.sender);
     }
 
     function _update(address to, uint256 tokenId, address auth) internal virtual returns (address) {
         address from = _ownerOf(tokenId);
 
-        // Perform (optional) operator check
         if (auth != address(0)) {
             _checkAuthorized(from, auth, tokenId);
         }
 
-        // Execute the update
         if (from != address(0)) {
-            // Clear approval. No need to re-authorize or emit the Approval event
             _approve(address(0), tokenId, address(0), false);
 
             unchecked {
@@ -314,9 +278,7 @@ contract ReportNFT is IERC721Extended {
         }
 
         _owners[tokenId] = to;
-
         emit Transfer(from, to, tokenId);
-
         return from;
     }
 
@@ -330,7 +292,35 @@ contract ReportNFT is IERC721Extended {
         }
     }
 
-    function share(uint256 tokenId, address targetWallet) external returns (uint256) {
+     function _isAuthorized(address tokenOwner, address spender, uint256 tokenId) internal view virtual returns (bool) {
+        return
+            spender != address(0) &&
+            (tokenOwner == spender || isApprovedForAll(tokenOwner, spender) || _getApproved(tokenId) == spender);
+    }
+
+    function _getApproved(uint256 tokenId) internal view virtual returns (address) {
+        return _tokenApprovals[tokenId];
+    }
+
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory data) private {
+        if (to.code.length > 0) {
+            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, data) returns (bytes4 retval) {
+                if (retval != IERC721Receiver.onERC721Received.selector) {
+                    revert ERC721InvalidReceiver(to);
+                }
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert ERC721InvalidReceiver(to);
+                } else {
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        }
+    }
+
+    function share(uint256 tokenId, address targetWallet) override external returns (uint256) {
         require(ownerOf(tokenId) == msg.sender, "Only the owner can share this report");
         require(targetWallet != address(0), "Target wallet cannot be zero");
 
@@ -342,26 +332,9 @@ contract ReportNFT is IERC721Extended {
 
         _mint(targetWallet, newTokenId);
 
-        emit ReportNFTMinted(targetWallet, newTokenId, originalReport.name, originalReport.startTime, originalReport.endTime);
+        emit ReportNFTShared(msg.sender, targetWallet, newTokenId);
 
         return newTokenId;
     }
 
-    function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
-        return _owners[tokenId];
-    }
-
-    function _isAuthorized(address tokenOwner, address spender, uint256 tokenId) internal view virtual returns (bool) {
-        return
-            spender != address(0) &&
-            (tokenOwner == spender || isApprovedForAll(tokenOwner, spender) || _getApproved(tokenId) == spender);
-    }
-
-    function isApprovedForAll(address tokenOwner, address operator) public view virtual returns (bool) {
-        return _operatorApprovals[tokenOwner][operator];
-    }
-
-    function _getApproved(uint256 tokenId) internal view virtual returns (address) {
-        return _tokenApprovals[tokenId];
-    }
 }
---
+++ src/services/blockchain/interfaces/ierc_isuncloud.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC_ISUNCLOUD {
+
+     struct Report {
+        string name;
+        uint256 startTime;
+        uint256 endTime;
+    }
+
+    event ReportNFTMinted(address indexed recipient, uint256 indexed newItemId, string reportName, uint256 startTime, uint256 endTime);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event ReportNFTShared(address indexed from, address indexed to, uint256 indexed tokenId);
+
+
+    error ERC721InvalidOwner(address owner);
+    error ERC721InvalidReceiver(address receiver);
+    error ERC721InvalidSender(address sender);
+    error ERC721IncorrectOwner(address owner, uint256 tokenId, address previousOwner);
+    error ERC721InvalidApprover(address approver);
+    error ERC721NonexistentToken(uint256 tokenId);
+    error ERC721InvalidOperator(address operator);
+    error ERC721InsufficientApproval(address spender, uint256 tokenId);
+
+    //Info: (20231229 - Yang){ supportsInterface(bytes4 interfaceId) is a function that is based on ERC165.}
+    //Info: (20231229 - Yang){ Checks if the contract supports a specific interface.}
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+    //Info: (20231229 - Yang){ the following functions are based on by ERC721.}
+    //Info: (20231229 - Yang){ Returns the name of the token.}
+    function name() external view returns (string memory);
+    //Info: (20231229 - Yang){ Returns the symbol of the token, usually a shorter version of the name.}
+    function symbol() external view returns (string memory);
+    //Info: (20231229 - Yang){ Returns the number of tokens owned by a given address.}
+    function balanceOf(address tokenOwner) external view returns (uint256);
+    //Info: (20231229 - Yang){ Returns the URI for a given token ID, usually pointing to a JSON file containing the token's metadata.}
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+    //Info: (20231229 - Yang){ Safely transfers a token from one address to another.}
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    //Info: (20231229 - Yang){ Safely transfers a token from one address to another, and includes an additional data parameter}
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) external;
+    //Info: (20231229 - Yang){ Returns the owner address of a given token ID.}
+    function ownerOf(uint256 tokenId) external view returns (address);
+    //Info: (20231229 - Yang){ Returns the owner address of the contract.}
+    function owner() external view returns (address);
+    //Info: (20231229 - Yang){ Approves a given address to manage a specific token ID.}
+    function approve(address to, uint256 tokenId) external;
+
+    //Info: (20231229 - Yang){ The following functions are created originally by isuncloud.}
+    //Info: (20231229 - Yang){ Shares a report by minting a new token for a specified wallet address.}
+    function share(uint256 tokenId, address targetWallet) external returns (uint256);
+    //Info: (20231229 - Yang){ Returns the report data for a given token ID.}
+    function getReportData(uint256 tokenId) external view returns (Report memory);
+    //Info: (20231229 - Yang){ Returns the latest token ID.}
+    function getLatestTokenID() external view returns (uint256);
+    //Info: (20231229 - Yang){ Mints a new report NFT and assigns it to the specified recipient.}
+    function mintReportNFT(address recipient, uint256 startTime, uint256 endTime, string memory reportName) external returns (uint256);
+
+
+}
